8209615eb1d083aff14a359d0d247ce473055584
SmartLearn <vivocoder@gmail.com>
2018-09-13 12:21:01 +0300

    DefaultValues

---
+++ defaultValues/bin/main.dart
@@ -0,0 +1,11 @@
+
+main(){
+
+  //declare variable mycount
+  int myCount;
+
+  assert(myCount == 'three');
+
+  print(myCount);
+
+}
---
+++ defaultValues/test/defaultValues_test.dart
@@ -0,0 +1,8 @@
+import 'package:defaultValues/defaultValues.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
